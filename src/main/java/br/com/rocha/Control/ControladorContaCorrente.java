091a465f7c8fc1448633cf5418dbb3bd418380ef
lucasflachr <lucasflachr@gmail.com>
2014-06-09 18:06:59 -0300

    correcao erro

--- src/main/java/br/com/rocha/Control/ControladorContaCorrente.java
+++ src/main/java/br/com/rocha/Control/ControladorContaCorrente.java
@@ -39,7 +39,7 @@ public class ControladorContaCorrente {
 			contaCorrente.setNumeroConta(numeroConta);
 			contaCorrente.setAgencia(codigoBanco);
 			contaCorrente.setDescricaoConta(descricao);			
-			//scontaCorrente.setSaldo(saldo);
+			contaCorrente.setSaldo(saldo);
 			
 			this.validarDadosInclusaoConta(contaCorrente);
 			
